cdee83c28bb7abe85232acc57e03d6205d9d4748
Yaacob Martinez <martinezyaacob@gmail.com>
2020-05-17 01:08:00 +0800

    tasker app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import axios from 'axios'
-import Home from './components/Home'
-import { ThemeProvider, createMuiTheme } from '@material-ui/core'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+import Home from "./components/Home";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 import { SWRConfig } from "swr";
-axios.defaults.baseURL = "https://api-iplus.herokuapp.com"
+axios.defaults.baseURL = `${process.env.API_URL}`;
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: "Poppins, san-serif"
-  }
-})
+	typography: {
+		fontFamily: "Poppins, san-serif",
+	},
+});
 
 function App() {
-  return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <SWRConfig value={{
-              fetcher: (url) => axios(url).then((r) => r.data),
-            }}>
-              <Route exact path="/" component={Home} />
-            </SWRConfig>
-          </Switch>
-        </Router>
-      </ThemeProvider>
-
-    </div>
-  );
+	return (
+		<div>
+			<ThemeProvider theme={theme}>
+				<Router>
+					<Switch>
+						<SWRConfig
+							value={{
+								fetcher: (url) => axios(url).then((r) => r.data),
+							}}>
+							<Route exact path='/' component={Home} />
+						</SWRConfig>
+					</Switch>
+				</Router>
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
